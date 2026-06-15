b94007573edf10a842a2fa7715354134b2ddb267
meeten <153901120+meeten@users.noreply.github.com>
2026-06-15 20:26:46 +0200

    Create :core:designsystem module

---
+++ core/designsystem/src/main/java/com/example/designsystem/Theme.kt
@@ -0,0 +1,24 @@
+package com.example.designsystem
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val AppColorScheme = darkColorScheme(
+    primary = Color.Black,
+    secondary = Color.Gray,
+
+    onPrimary = Color.White
+)
+
+@Composable
+fun GitHubClientTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = AppColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
