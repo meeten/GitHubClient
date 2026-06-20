ec16fd3f5341dbc483dd97ebb2d2fbe55cb2a7be
meeten <153901120+meeten@users.noreply.github.com>
2026-06-20 13:46:30 +0200

    Add compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
 
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
