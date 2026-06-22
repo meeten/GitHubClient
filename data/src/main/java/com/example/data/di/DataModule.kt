565ac8bd712ffbbfeb64fa0572bd38363a14e72f
meeten <153901120+meeten@users.noreply.github.com>
2026-06-22 15:56:32 +0200

    Add factory for GitHubMapper in data module

--- data/src/main/java/com/example/data/di/DataModule.kt
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -1,11 +1,15 @@
 package com.example.data.di
 
 import com.example.data.network.GitHubRemoteDataSource
+import com.example.data.network.mapper.GitHubMapper
 import com.example.data.repository.GitHubRepositoryImpl
 import com.example.domain.repository.GitHubRepository
 import org.koin.dsl.module
 
 val dataModule = module {
+
+    factory { GitHubMapper() }
+
     single {
         GitHubRemoteDataSource(
             networkClient = get(),
