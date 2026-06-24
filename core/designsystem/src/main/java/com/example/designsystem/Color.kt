0ac37607d2f4b3d9df74003473678f4fab1e4cb8
meeten <153901120+meeten@users.noreply.github.com>
2026-06-24 15:09:45 +0200

    Update theme palette and introduce custom shades

--- core/designsystem/src/main/java/com/example/designsystem/Color.kt
+++ core/designsystem/src/main/java/com/example/designsystem/Color.kt
@@ -2,8 +2,14 @@ package com.example.designsystem
 
 import androidx.compose.ui.graphics.Color
 
+internal val black800 = Color(0xFF0D1117)
+internal val black600 = Color(0xFF161b22)
+internal val grey600 = Color(0xFF21262D)
+
 internal val green600 = Color(0xFF438440)
 
 internal val blue600 = Color(0xFF58A6FF)
 
 internal val red600 = Color(0xFFB00020)
+
+internal val red400 = Color(0xFFCB4F4F)
