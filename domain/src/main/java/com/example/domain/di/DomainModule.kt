79fda9b063d3eb4d1b4e21393eca33269c4f9261
meeten <153901120+meeten@users.noreply.github.com>
2026-06-24 15:11:30 +0200

    Register GetReposUseCase in domainModule DI

--- domain/src/main/java/com/example/domain/di/DomainModule.kt
+++ domain/src/main/java/com/example/domain/di/DomainModule.kt
@@ -1,8 +1,10 @@
 package com.example.domain.di
 
+import com.example.domain.usecase.GetReposUseCase
 import com.example.domain.usecase.ValidateTokenUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
     factory { ValidateTokenUseCase(repository = get()) }
+    factory { GetReposUseCase(repository = get()) }
 }
