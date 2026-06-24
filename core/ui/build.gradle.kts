f0c9038621cac58ad1943fa4186d99ac87d00b18
meeten <153901120+meeten@users.noreply.github.com>
2026-06-24 15:12:09 +0200

    Add :core:designsystem in :core:ui module

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:designsystem"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
