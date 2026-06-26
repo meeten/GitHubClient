51274862775299da76379163c33109d61245b275
meeten <153901120+meeten@users.noreply.github.com>
2026-06-26 15:24:09 +0200

    Make license nullable in RepoDetailsDto to prevent serialization crash

--- data/src/main/java/com/example/data/network/model/RepoDetailsDto.kt
+++ data/src/main/java/com/example/data/network/model/RepoDetailsDto.kt
@@ -9,7 +9,7 @@ data class RepoDetailsDto(
     @SerialName("owner") val owner: OwnerDto,
     @SerialName("name") val name: String,
     @SerialName("html_url") val url: String,
-    @SerialName("license") val license: LicenseDto,
+    @SerialName("license") val license: LicenseDto?,
     @SerialName("stargazers_count") val stars: Int,
     @SerialName("forks_count") val forks: Int,
     @SerialName("watchers_count") val watchers: Int,
