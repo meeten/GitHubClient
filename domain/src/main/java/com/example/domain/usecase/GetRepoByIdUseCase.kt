22c6ad67539616fa45c3d9e5117c842e4b27a121
meeten <153901120+meeten@users.noreply.github.com>
2026-06-26 14:50:55 +0200

    Rename param hitHubRepository to repository in GetRepoByIdUseCase

--- domain/src/main/java/com/example/domain/usecase/GetRepoByIdUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetRepoByIdUseCase.kt
@@ -1,14 +1,14 @@
 package com.example.domain.usecase
 
 import com.example.domain.model.OperationResult
-import com.example.domain.model.Repo
+import com.example.domain.model.RepoDetails
 import com.example.domain.repository.GitHubRepository
 
 class GetRepoByIdUseCase(
-    private val hitHubRepository: GitHubRepository
+    private val repository: GitHubRepository
 ) {
 
-    suspend operator fun invoke(id: Int): OperationResult<Repo> {
-        return hitHubRepository.getRepo(id)
+    suspend operator fun invoke(id: Int): OperationResult<RepoDetails> {
+        return repository.getRepo(id)
     }
 }
